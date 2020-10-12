210a8771a5b6ca94955dcc7b6d7850e7b92c1eb3
shubham bhardwaj <tom@gmail.com>
2020-10-12 16:01:46 -0400

    New file

---
+++ chromeos.sh
@@ -0,0 +1,18 @@
+echo "What is your name?"
+read Person 
+echo "Hello,$Person"
+
+echo "Where are you from?"
+read Place
+echo "North or South ?"
+echo "Which State?"
+read State
+echo "Nice State"
+echo "Does it get hot there?"
+if [ "Temprature" -gt "35" ]
+then 
+	echo "Yes! it's very hot"
+else 
+	echo "No! it's alright"
+fi
+
